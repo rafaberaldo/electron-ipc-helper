34697bfa5a4b35c16a0fc7cbf1c7cd1ee87d8662
Rafael Beraldo <rafael.pimpa@gmail.com>
2022-08-04 11:43:57 -0300

    update ipc

--- ipc.ts
+++ ipc.ts
@@ -1,6 +1,6 @@
 import { ipcMain, ipcRenderer } from 'electron'
 
-function checkKey (key?: string) {
+function validateKey (key?: string) {
   if (!key || key === 'anonymous') {
     throw new Error('Key is required for anonymous methods.')
   }
@@ -8,10 +8,13 @@ function checkKey (key?: string) {
 
 /**
  * Create async pair.
+ * @param fn the method/function
+ * @param key optional for named functions, required for anonymous
+ * @returns object { invoker, handler }
  */
 export function createIpcPair<T extends (...args: any[]) => any> (fn: T, key?: string) {
   const newKey = key ?? fn.name
-  checkKey(newKey)
+  validateKey(newKey)
 
   const invoker = (...args: Parameters<T>): Promise<ReturnType<T>> => {
     const response = ipcRenderer.invoke(newKey, ...args)
@@ -19,10 +22,10 @@ export function createIpcPair<T extends (...args: any[]) => any> (fn: T, key?: s
     return response
   }
 
-  const handler = (...argsOuter: any[]) => {
-    ipcMain.handle(newKey, (event, ...argsInner) => {
-      // Todo: validate event.sender
-      return fn(...argsInner, ...argsOuter)
+  const handler = (...argsMain: any[]) => {
+    ipcMain.handle(newKey, (event, ...argsRenderer) => {
+      // Todo: validate sender
+      return fn(...argsRenderer, ...argsMain)
     })
   }
 
@@ -34,16 +37,19 @@ export function createIpcPair<T extends (...args: any[]) => any> (fn: T, key?: s
 
 /**
  * Create sync pair. Only works with sync methods, should use async when possible.
+ * @param fn the method/function
+ * @param key optional for named functions, required for anonymous
+ * @returns object { invoker, handler }
  */
 export function createIpcPairSync<T extends (...args: any[]) => any> (fn: T, key?: string) {
   const newKey = key ?? fn.name
-  checkKey(newKey)
+  validateKey(newKey)
 
   const invoker = <T>((...args) => ipcRenderer.sendSync(newKey, ...args))
 
-  const handler = (...argsOuter: any[]) => ipcMain.on(newKey, (event, ...argsInner) => {
-    // Todo: validate event.sender
-    event.returnValue = fn(...argsInner, ...argsOuter)
+  const handler = (...argsMain: any[]) => ipcMain.on(newKey, (event, ...argsRenderer) => {
+    // Todo: validate sender
+    event.returnValue = fn(...argsRenderer, ...argsMain)
   })
 
   return {
